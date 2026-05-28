6dd8a3fbc0fe5344303f5ae67d83b5062107b545
John <jonathan.vergaran@alumno.buap.mx>
2026-05-27 22:07:12 -0600

    Se realizarón correcciones en ms-reportes

--- ms-reportes/dist/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from './app.service';
-import type { Response } from 'express';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    descargarReporteCalificaciones(nrc: string, res: Response): Promise<void>;
-}
